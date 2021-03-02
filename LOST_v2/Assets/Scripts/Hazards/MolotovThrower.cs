b2d9f47f4dc7f458f195f2c25d7acf0b429cefc6
Devon Garrett <devgarre@uat.edu>
2021-03-01 18:17:01 -0700

    Added Colosseum

--- LOST_v2/Assets/Scripts/Hazards/MolotovThrower.cs
+++ LOST_v2/Assets/Scripts/Hazards/MolotovThrower.cs
@@ -12,6 +12,7 @@ public class MolotovThrower : MonoBehaviour
     public float minSpeed;
     public GameObject objectToSpawn;
     public Transform spawnLocation;
+    public float startDelay;
     public float maxTimeToSpawn;
     public float minTimeToSpawn;
     private float timer;
@@ -19,7 +20,7 @@ public class MolotovThrower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = Random.Range(minTimeToSpawn, maxTimeToSpawn);
+        timer = startDelay;
         if (spawnLocation == null)
         {
             spawnLocation = gameObject.transform;
@@ -40,15 +41,15 @@ public class MolotovThrower : MonoBehaviour
         }
         else
         {
+            timer = Random.Range(minTimeToSpawn, maxTimeToSpawn);
+
             GameObject spawnedObject = Instantiate(objectToSpawn, spawnLocation.position, spawnLocation.rotation);
             Quaternion tempRot = Quaternion.Euler(Random.Range(minVertAngle, maxVertAngle) * -1, Random.Range(minHorizAngle, maxHorizAngle), 0);
             float tempSpeed = Random.Range(minSpeed, maxSpeed);
-            spawnedObject.GetComponent<Rigidbody>().AddForce(tempRot * Vector3.forward * tempSpeed);
-            spawnedObject.GetComponent<Rigidbody>().AddTorque(Quaternion.Euler(0, tempRot.eulerAngles.y, 0) * Vector3.right * tempSpeed);
+            spawnedObject.GetComponent<Rigidbody>().AddForce(tempRot * spawnLocation.forward * tempSpeed);
+            spawnedObject.GetComponent<Rigidbody>().AddTorque(Quaternion.Euler(0, tempRot.eulerAngles.y, 0) * spawnLocation.right * tempSpeed);
 
-            Debug.DrawLine(spawnLocation.position, spawnLocation.position + (tempRot * Vector3.forward), Color.blue, 2);
-
-            timer = Random.Range(minTimeToSpawn, maxTimeToSpawn);
+            Debug.DrawLine(spawnLocation.position, spawnLocation.position + (tempRot * spawnLocation.forward), Color.blue, 2);
         }
     }
 }
